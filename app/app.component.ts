01bbcac6b8bf93282d9298d1bdaba44dea72d8a0
Taylor Grohs <taylorgrohs@gmail.com>
2016-03-24 19:28:28 -0700

    add app.component

---
+++ app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter } from 'angular2/core';
+import { MealListComponent } from './meal-list.component';
+import { Meal } from './meal.model';
+import { NewMealComponent } from './new-meal.component';
+
+@Component({
+  selector: 'my-app',
+  directives: [MealListComponent],
+  template: `
+    <div class="container">
+      <h1>Meal Tracker</h1>
+      <meal-list
+      [mealList]="meals"
+      (onMealSelect)="mealWasSelected($event)">
+      </meal-list>
+    </div>
+  `
+})
+export class AppComponent {
+  public meals: Meal[];
+  constructor() {
+    this.meals[
+      new Meal("Muffin", "Quick breakfast", 300),
+      new Meal("Chicken Tacos", "Food cart lunch", 500),
+      new Meal("CheeseBurger", "Dinner", 1000)
+    ];
+  }
+  mealWasSelected(clickedMeal: Meal): void {
+    
+  }
+}
